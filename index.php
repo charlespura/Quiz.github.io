c2916a9b62ef94c93e4e6fb3674a72910ecb958c
Charles <charles051902pura@gmail.com>
2025-04-14 01:12:36 +0800

    Initial commit of Quiz App

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<?php include "navbar.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Quiz App Landing Page</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+
+
+  <div class="content-container">
+    <div class="container">
+      <h1>Welcome to QuizTime!</h1>
+      <p>Test your knowledge, challenge your brain, and have fun while learning. Ready to get started?</p>
+      <a href="quiz.html" class="btn"> Login</a>
+    </div>
+  </div>
+
+</body>
+</html>
